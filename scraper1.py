7230da1db09bc630910d9300ede8a3ddd73b52c1
Christopher Ullery <cullery@couriertimes.com>
2023-06-04 19:32:55 -0400

    Add files via upload

--- scraper1.py
+++ scraper1.py
@@ -3,6 +3,8 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import pdb
 from datetime import date
+import glob 
+import os
 import warnings
 warnings.filterwarnings('ignore')
 
@@ -107,8 +109,28 @@ df1=pd.concat([df1,df2])
     #df2=pd.concat([df1,df])
 # df2 = pd.concat(df)
 df1=df1[df1.title!='---']
-df1.to_csv("bookLooks_"+str(today.strftime("%m_%d_%Y")+".csv"),index=None)
+df1.reset_index(inplace=True)
+df1.drop(columns={'index'},inplace=True)
+df1['retrieved_date']=today.strftime("%m/%d/%Y")
+df1.to_csv("booklooks_lists//bookLooks_"+str(today.strftime("%m_%d_%Y")+".csv"),index=None)
+path = 'booklooks_lists/*.csv'
+list_of_files = glob.glob(path)
+sorted_files = sorted(list_of_files, key=os.path.getctime)
+last_file = pd.read_csv(sorted_files[-2])
+first_file=pd.read_csv(sorted_files[0])
+first_file = first_file[~first_file.title.str.contains("http")]
 pct_over3=((df1[df1.rating.astype(int)>=3].title.count())/(df1.title.count())).round(2)*100
 print("There are "+str(len(df1.title))+" books listed on BookLooks as of "+str(today.strftime("%m/%d/%Y"))+".")
-print("Roughly "+str(pct_over3)+"% of those titles are rated three or higher.")
+print("There were "+str(len(last_file.title))+" files as of "+str(last_file.retrieved_date[0])+".")
+print(df1[~df1.title.isin(last_file.title)].title.to_list()[0]+" by "+df1[~df1.title.isin(last_file.title)].author.to_list()[0]+" was added since that previous scrape.")
+print("There have been "+str(df1[~df1.title.isin(first_file.title)].title.count())+" books added since "+first_file.retrieved_date[0])
+print("Roughly "+str(pct_over3)+"% of BookLooks' current titles are rated three or higher.")
 print(str(df1.author.value_counts().index[0])+" is the most frequently mentioned author with "+str(df1.author.value_counts()[0])+" titles in this list. \n")
+url = 'https://en.wikipedia.org/wiki/List_of_most_commonly_challenged_books_in_the_United_States'
+headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'}
+r = requests.get(url=url, headers=headers)
+soup = BeautifulSoup(r.content,'html')
+table=soup.find("tbody")
+table=pd.read_html(url)[0]
+table.columns=['title','author','reasons_for_challenge','years_published','alaRank10_19','alaRank00_09','alaRank90_99']
+table[table.title.str.contains("Gender")]
